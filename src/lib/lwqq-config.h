a384f196b7367e33970239a3dd811413ae1e0ffe
Loftor <i@loftor.com>
2014-01-22 15:44:58 +0800

    init

---
+++ src/lib/lwqq-config.h
@@ -0,0 +1,14 @@
+#ifndef _LWQQ_CONFIG_H__
+#define _LWQQ_CONFIG_H__
+
+#define WITH_LIBEV
+/* #undef WITH_LIBUV */
+#define WITH_SQLITE
+/* #undef WITHOUT_ASYNC */
+#define SSL
+/* #undef WITH_MOZJS */
+/* #undef MOZJS_185 */
+/* #undef MOZJS_17 */
+
+#endif /* __CONFIG_H__ */
+
